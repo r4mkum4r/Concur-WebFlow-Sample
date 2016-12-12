386e40cf578c465c02a72ea88022564b48d2ac43
raramakrishna <raramakrishna@egencia.com>
2016-12-12 22:07:09 +0530

    CORS setup

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ concur.oauth.native(config.concur)
   console.log(error);
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('test test');
 });
